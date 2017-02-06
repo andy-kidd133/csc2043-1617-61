29282a2ffc8accb854f5ae80889e112d71825f19
andy-kidd133 <akidd03@qub.ac.uk>
2017-02-06 15:53:01 +0000

    MainActivity 'window' editing

--- app/src/main/java/com/example/andrew/ark9studios/MainActivity.java
+++ app/src/main/java/com/example/andrew/ark9studios/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.andrew.ark9studios;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
         setContentView(R.layout.activity_main);
     }
-
-    
 }
