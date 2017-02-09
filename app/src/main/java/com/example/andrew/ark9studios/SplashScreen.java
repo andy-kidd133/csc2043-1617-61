149e98ec1095deb3b8ad528f14d28205ac5b7b24
megan <mokane638@qub.ac.uk>
2017-02-09 13:28:08 +0000

    Splash screen class and xml file

---
+++ app/src/main/java/com/example/andrew/ark9studios/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.andrew.ark9studios;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by megan on 09/02/2017.
+ */
+
+public class SplashScreen extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
