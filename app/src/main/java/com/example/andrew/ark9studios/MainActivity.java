08dbc7feb8b06ba9c9a29340a0cb10f11a2c020e
Emma <emcallister41@qub.ac.uk>
2017-02-28 17:09:50 +0000

    got cards on screen

--- app/src/main/java/com/example/andrew/ark9studios/MainActivity.java
+++ app/src/main/java/com/example/andrew/ark9studios/MainActivity.java
@@ -2,10 +2,19 @@ package com.example.andrew.ark9studios;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+
+    ImageView deck,card1,card2,card3,card4,card5,card6;
+
+    ArrayList<Integer> cards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,23 @@ public class MainActivity extends Activity {
         
         
         setContentView(R.layout.main_menu);
+
+
+        deck = (ImageView) findViewById(R.id.deck);
+        card1 = (ImageView) findViewById(R.id.card1);
+        card2 = (ImageView) findViewById(R.id.card2);
+        card3 = (ImageView) findViewById(R.id.card3);
+        card4 = (ImageView) findViewById(R.id.card4);
+        card5 = (ImageView) findViewById(R.id.card5);
+        card6 = (ImageView) findViewById(R.id.card6);
+
+        card1.setVisibility(View.INVISIBLE);
+        card2.setVisibility(View.INVISIBLE);
+        card3.setVisibility(View.INVISIBLE);
+        card4.setVisibility(View.INVISIBLE);
+        card5.setVisibility(View.INVISIBLE);
+        card6.setVisibility(View.INVISIBLE);
+
+
     }
 }
