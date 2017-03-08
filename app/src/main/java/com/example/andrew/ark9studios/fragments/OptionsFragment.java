14769842d5e0253b15ada00b0f2127838ce7bdc2
Megan O'kane <mokane638@qub.ac.uk>
2017-03-08 19:36:09 +0000

    Logo for ark9 studios made and implemented along with the unimon logo

--- app/src/main/java/com/example/andrew/ark9studios/fragments/OptionsFragment.java
+++ app/src/main/java/com/example/andrew/ark9studios/fragments/OptionsFragment.java
@@ -1,10 +1,39 @@
 package com.example.andrew.ark9studios.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andrew.ark9studios.R;
 
 /**
  * Created by megan on 02/03/2017.
  */
 
 public class OptionsFragment extends Fragment {
+
+    //TODO : CREATE METHOD FOR RADIO BUTTONS WHEN MUSIC IS IMPLEMENTED
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+    View view = inflater.inflate(R.layout.options_frag, container, false);
+
+
+
+
+
+
+
+        return view;
+
+
+    }
+
+
+
 }
