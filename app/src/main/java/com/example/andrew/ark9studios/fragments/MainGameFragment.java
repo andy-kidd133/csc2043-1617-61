7e5e0ae71c4fbb1b64d3d4ccb26a9f4f5f9dd1d3
Megan O'kane <mokane638@qub.ac.uk>
2017-03-14 20:33:03 +0000

    player deck and enemy deck images put onto main game xml

--- app/src/main/java/com/example/andrew/ark9studios/fragments/MainGameFragment.java
+++ app/src/main/java/com/example/andrew/ark9studios/fragments/MainGameFragment.java
@@ -8,13 +8,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+import com.example.andrew.ark9studios.AssetStore;
+import com.example.andrew.ark9studios.GameLoop;
+import com.example.andrew.ark9studios.GameMusic;
 import com.example.andrew.ark9studios.R;
+import com.example.andrew.ark9studios.activities.MainActivity;
 
 /**
  * Created by Megan on 06/03/2017.
  */
 
 public class MainGameFragment extends Fragment {
+
+    private static GameLoop gameLoop;
+    private AssetStore assetStore;
+    private MainActivity mainActivity;
+    private static GameMusic gameMusic;
 
 
 
@@ -23,6 +32,10 @@ public class MainGameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_game_frag, container, false);
 
+
+        mainActivity = (MainActivity)getActivity();
+
+        gameMusic = new GameMusic(getActivity().getApplicationContext(), R.raw.laser_groove);
 
 
 
