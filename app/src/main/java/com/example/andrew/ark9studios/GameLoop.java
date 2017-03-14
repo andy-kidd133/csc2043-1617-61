a0c2387136127ca9dabc75548c0707fa095122e2
Megan O'kane <mokane638@qub.ac.uk>
2017-03-14 19:55:54 +0000

    Game loop and Asset store for loading bitmaps done

--- app/src/main/java/com/example/andrew/ark9studios/GameLoop.java
+++ app/src/main/java/com/example/andrew/ark9studios/GameLoop.java
@@ -1,202 +1,151 @@
 package com.example.andrew.ark9studios;
 
+import android.util.Log;
+
 /**
- * Created by Andrew on 08/02/2017.
+ * Created by Megan on 14/11/2016.
  */
 
-public class GameLoop  {
-
-   /* //instance of game loop
-    private GameLoop mLoop;
-
-    //target FPS
-    private int mTargetFramesPerSeconds = 5;
-
-    //average FPS
-    private float mAverageFramesPerSecond;
+public abstract class GameLoop implements Runnable {
 
 
+    //Instance variables
 
-    /////////////////////////////////////////////////////////////////////
-    // Properies
-    /////////////////////////////////////////////////////////////////////
+    private int targetFramesPerSecond;
+    private Thread renderThread = null;
+    private volatile boolean isRunning = false;
+    private long targetUpdatePeriod;
 
-    // Concurrent lock that can be used to control update and draws inter-thread sequencing
 
-    class BooleanLock {
-        boolean isLocked;
+    /**
+     * This is the constructor for the game loop
+     *
+     * @param targetFPS this is the frames per second that we want our app to run at
+     */
 
-        public BooleanLock(boolean isLocked) {
-            this.isLocked = isLocked;
-        }
-    }
-
-    //Sequence locks for update and draw steps
-
-    volatile BooleanLock update;
-    volatile BooleanLock draw;
-
-    //thread on which the game loop will run
-
-    Thread renderThread = null;
-
-    ElapsedTime elapsedTime;
-
-    long targetStepPeriod;
-
-    //provides ceiling on the max step size that will be reported to game objects
-
-    double maximumStepPeriodScale = 3.0f;
-
-    /////////////////////////////////////////////////////////////////////
-    // Constructor
-    /////////////////////////////////////////////////////////////////////
-
-    public GameLoop() {
-        //setup the target step period
-        targetStepPeriod = 1000000000 / mTargetFramesPerSeconds;
-        //create a new time structure
-        elapsedTime = new ElapsedTime();
-        //create new update and draw locks
-        update = new BooleanLock(false);
-        draw = new BooleanLock(false);
+    public GameLoop(int targetFPS) {
+        targetFramesPerSecond = targetFPS;
+        targetUpdatePeriod = 1000000000 / targetFramesPerSecond;
+        run();
     }
 
 
-    ////////////////////////////////////////////////////////////////////////
-    // Run method - update and draw loop
-    ////////////////////////////////////////////////////////////////////////
-
+    @Override
     public void run() {
-
-        //insert 'if' statement to ensure there is a game screen available
-
-        long startRun;
-        long startStep, endStep;
+        long startTime, endTime;
         long sleepTime, overSleepTime;
-        boolean running = false;
+        long timeBeyondPeriod;
 
-        startRun = System.nanoTime() - targetStepPeriod;
-        startStep = startRun;
-        overSleepTime = 0L;
 
-        //insert try-catch???
+        startTime = System.nanoTime() - targetUpdatePeriod;
 
-        while (running){
+        overSleepTime = 0;
+        timeBeyondPeriod = 0;
 
-            //update the timing info
-            long currentTime = System.nanoTime();
-            elapsedTime.totalTime = (currentTime - startRun) / 1000000000.0;
-            elapsedTime.stepTime = (currentTime - startStep) / 1000000000.0;
-            startStep = currentTime;
 
-            //weighted average update of the average FPS
-            mAverageFramesPerSecond = 0.85f * mAverageFramesPerSecond
-                    + 0.15f * (1.0f / (float) elapsedTime.stepTime);
+        //the main loop that deals with FPS
+        try {
+            while (isRunning) {
+                startTime = System.nanoTime();
 
-            //ensure the reported step time is not abnormally large
-            if (elapsedTime.stepTime > (targetStepPeriod / 1000000000.0) * maximumStepPeriodScale)
-                elapsedTime.stepTime = (targetStepPeriod / 1000000000.0) * maximumStepPeriodScale;
+                doUpdate();
+                doDraw();
 
-            //trigger an update
-            synchronized (update) {
-                update.isLocked = true;
-            }
-            doUpdate(elapsedTime);
+                endTime = System.nanoTime();
+                sleepTime = (targetUpdatePeriod - (startTime - endTime)) - overSleepTime;
 
-            //wait for the update to complete before processing
-            synchronized (update) {
-                if (update.isLocked) {
-                    update.wait();
+
+                if (sleepTime > 0) {
+                    Thread.sleep(sleepTime / 10000000L);
+
+                    overSleepTime = (System.nanoTime() - endTime) - sleepTime;
+                } else {
+
+                    timeBeyondPeriod = timeBeyondPeriod - sleepTime;
+                    overSleepTime = 0;
+
+                    while (timeBeyondPeriod > targetUpdatePeriod) {
+                        //doUpdate();
+                        timeBeyondPeriod = -targetUpdatePeriod;
+                    }
                 }
             }
-
-            //trigger a draw request
-            synchronized (draw) {
-                draw.isLocked = true;
-            }
-            doDraw(elapsedTime);
-
-            //wait for the draw to complete before processing
-            synchronized (draw) {
-                if (draw.isLocked) {
-                    draw.wait();
-                }
-            }
-
-            //measure how long the update/draw took to complete
-            //calculate how long to sleep for
-
-            endStep = System.nanoTime();
-            sleepTime = (targetStepPeriod - (endStep - startRun))
-                    - overSleepTime;
-
-            //if needed put the thread to sleep
-            if (sleepTime > 0) {
-                Thread.sleep(sleepTime / 1000000L);     //convert ns to ms
-
-                //determine over sleep time
-                overSleepTime = (System.nanoTime() -endStep)
-                        - sleepTime;
-            } else {
-                overSleepTime = 0L;
-            }
-        }
-
-    }
-
-    public void doUpdate(){
-
-    }
-
-    public void doDraw() {
-
-    }
-
-    //notify the game loop that the update has completed
-
-    public void notifyDrawCompleted() {
-        synchronized (draw) {
-            draw.isLocked = false;
-            draw.notifyAll();
+        } catch (InterruptedException e) {
+            Log.w("Loop", "Interrupt has occured");
         }
     }
 
-    //notify the game loop that the update as completed
 
-    public void notifyUpdateCompleted() {
-        synchronized (update) {
-            update.isLocked = false;
-            update.notifyAll();
-        }
-    }
 
-    ///////////////////////////////////////////////////////////////////
-    // Pause/Resume methods for the game loop
-    ///////////////////////////////////////////////////////////////////
 
+    /**
+     * This is the abstract draw method which will get overridden in the main game fragment
+     */
+    public abstract void doDraw();
+
+    /**
+     * This is the abstract update method which will get overridden in the main game fragment
+     */
+    public abstract void doUpdate();
+
+    /**
+     *
+     * This pause method is used to pause the game loop and can be called from the main
+     * game fragment
+     * it sets the isRunning to false and removes the other thread
+     */
     public void pause() {
-        running = false;
+    isRunning= false;
+
         while (true) {
             try {
                 renderThread.join();
                 return;
             } catch (InterruptedException e) {
-
+                Log.e("Game Loop", "pauing the render thread failed");
             }
         }
     }
 
+    /**
+     * The resume method has to create a new instance of the thread because of the way we deal with pausing
+     */
     public void resume() {
-        running = true;
-
-        draw.isLocked = false;
-        update.isLocked = false;
+        isRunning = true;
 
         renderThread = new Thread(this);
         renderThread.start();
     }
-*/
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
