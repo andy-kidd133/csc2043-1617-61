2e7f07a3d2727664358ab9e2b37fe6413138007a
Karl McCoubrey <kmccoubrey02@qub.ac.uk>
2017-04-19 20:31:09 +0100

    Beginning to populate game screen along with linking to menu button

--- app/src/main/java/com/example/andrew/ark9studios/fragments/MainGameScreen.java
+++ app/src/main/java/com/example/andrew/ark9studios/fragments/MainGameScreen.java
@@ -1,10 +1,17 @@
 package com.example.andrew.ark9studios.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+
 import com.example.andrew.ark9studios.AssetManager;
 import com.example.andrew.ark9studios.Game;
 import com.example.andrew.ark9studios.GameGraphics.Graphics2DInterface;
+import com.example.andrew.ark9studios.GameInput.GameTouchEvent;
+import com.example.andrew.ark9studios.GameInput.Input;
 import com.example.andrew.ark9studios.GameScreen;
 import com.example.andrew.ark9studios.gameInfrastructure.ElapsedTime;
+
+import java.util.List;
 
 /**
  * Created by megan on 15/04/2017.
@@ -14,6 +21,16 @@ public class MainGameScreen extends GameScreen {
 
     public static final String SCREEN_NAME = "MainGameScreen";
 
+    private Rect card1Bound;
+    private Rect backgroundBound;
+
+    private Bitmap card1Bitmap;
+    private Bitmap backgroundBitmap;
+
+    private static final int CARD_WIDTH = 150;
+    private static final int CARD_HEIGHT = 150;
+
+
 
    public MainGameScreen(Game game){
        super(SCREEN_NAME, game);
@@ -21,6 +38,11 @@ public class MainGameScreen extends GameScreen {
        AssetManager assetManager = game.getAssetManager();
        assetManager.emptyAssets();
 
+       assetManager.loadAndAddBitmap("card1", "drawable/card1.png");
+       assetManager.loadAndAddBitmap("backgroundLayer", "images/qubbg.png");
+
+       this.card1Bitmap = assetManager.getBitmap("card1");
+       this.backgroundBitmap = assetManager.getBitmap("backgroundLayer");
 
    }
 
@@ -29,11 +51,29 @@ public class MainGameScreen extends GameScreen {
 
     @Override
     public void update(ElapsedTime elapsedTime) {
+        Input input = game.getInput();
+        List<GameTouchEvent> touchEvents = input.getTouchEvents();
 
     }
 
     @Override
     public void draw(ElapsedTime elapsedTime, Graphics2DInterface graphics2DInterface) {
+
+
+
+        if(backgroundBound == null){
+            backgroundBound = new Rect(0, 0, graphics2DInterface.getSurfaceWidth(),
+                    graphics2DInterface.getSurfaceHeight());
+        }
+
+        if(card1Bound == null){
+            card1Bound = new Rect(20, 20, 20 + CARD_WIDTH,
+                    20 + CARD_HEIGHT);
+        }
+
+
+        graphics2DInterface.drawBitmap(backgroundBitmap, null, backgroundBound, null);
+        graphics2DInterface.drawBitmap(card1Bitmap, null, card1Bound, null);
 
     }
 
